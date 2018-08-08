8034a295bd1d929db289149cbdc8efe34f8a4904
ferdous-al-quraeshi <ecopavel@gmail.com>
2018-08-08 16:44:45 +0600

    setting attributes

--- src/components/counterComponet.jsx
+++ src/components/counterComponet.jsx
@@ -3,14 +3,16 @@ import React, { Component } from "react";
 class Counter extends Component {
   // states of this component
   state = {
-    count: 0
+    count: 0,
+    imageURL: "https://picsum.photos/78"
   };
 
   render() {
     return (
       <React.Fragment>
-        <span> {this.formatCount()}</span>
-        <button>Increment</button>
+        <img src={this.state.imageURL} alt="" />
+        <span className="badge badge-primary m-2"> {this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
