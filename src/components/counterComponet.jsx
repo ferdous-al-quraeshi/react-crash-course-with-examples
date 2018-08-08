f23378733a6e2debb2cf3ffa55260f9a051c6a86
ferdous-al-quraeshi <ecopavel@gmail.com>
2018-08-08 18:29:42 +0600

    rendering lists

--- src/components/counterComponet.jsx
+++ src/components/counterComponet.jsx
@@ -3,20 +3,39 @@ import React, { Component } from "react";
 class Counter extends Component {
   // states of this component
   state = {
-    count: 0
+    count: 0,
+    tags: [
+      {
+        id: 1,
+        el: "tag1"
+      },
+      {
+        id: 2,
+        el: "tag1"
+      },
+      {
+        id: 3,
+        el: "tag1"
+      },
+      {
+        id: 4,
+        el: "tag1"
+      }
+    ]
   };
 
   render() {
     return (
       <React.Fragment>
-        <span className={this.getClasses()}> {this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>{this.state.tags.map(t => <li key={t.id}>{t.el}</li>)}</ul>
       </React.Fragment>
     );
   }
 
   // helper methods
-  getClasses() {
+  getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
