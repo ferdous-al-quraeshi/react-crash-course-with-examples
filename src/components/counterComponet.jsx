5789521c1c892e1f4e103c1a7cd179d3ec059a00
ferdous-al-quraeshi <ecopavel@gmail.com>
2018-08-08 19:40:22 +0600

    conditional rendering

--- src/components/counterComponet.jsx
+++ src/components/counterComponet.jsx
@@ -5,22 +5,22 @@ class Counter extends Component {
   state = {
     count: 0,
     tags: [
-      {
-        id: 1,
-        el: "tag1"
-      },
-      {
-        id: 2,
-        el: "tag1"
-      },
-      {
-        id: 3,
-        el: "tag1"
-      },
-      {
-        id: 4,
-        el: "tag1"
-      }
+      // {
+      //   id: 1,
+      //   el: "tag1"
+      // },
+      // {
+      //   id: 2,
+      //   el: "tag1"
+      // },
+      // {
+      //   id: 3,
+      //   el: "tag1"
+      // },
+      // {
+      //   id: 4,
+      //   el: "tag1"
+      // }
     ]
   };
 
@@ -29,12 +29,26 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>{this.state.tags.map(t => <li key={t.id}>{t.el}</li>)}</ul>
+        {this.renderTags()}
+        {/* following is a conditional expression within 'jsx'5 */}
+        {this.state.tags.length === 0 && (
+          <p className="text-warning">Put some tags here...</p>
+        )}
       </React.Fragment>
     );
   }
 
   // helper methods
+  renderTags() {
+    if (this.state.tags.length === 0)
+      return (
+        <p>
+          No tags found!
+          <span className="ml-2 text-warning">Put some tags here...</span>
+        </p>
+      );
+    return <ul>{this.state.tags.map(t => <li key={t.id}>{t.el}</li>)}</ul>;
+  }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
