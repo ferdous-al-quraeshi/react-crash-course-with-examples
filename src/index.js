ddd29b6141a4dd5586336bc202113c3e3a3bc4c3
ferdous-al-quraeshi <ecopavel@gmail.com>
2018-08-08 16:06:27 +0600

    created (and successfully rendered) first 'Counter' component

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import Counter from "./components/counterComponet";
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
 registerServiceWorker();
