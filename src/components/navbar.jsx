b67f63419270ace62c4747bafddfa0e762ece5ad
ferdous-al-quraeshi <ecopavel@gmail.com>
2018-08-10 15:46:31 +0600

    Stateless Functional Components (NavBar)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component
+const NavBar = ({ totalCounters }) => {
+  // 'Object destructuring' applied
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <span className="navbar-brand">
+        Total Counters{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </span>
+    </nav>
+  );
+};
 
 export default NavBar;
