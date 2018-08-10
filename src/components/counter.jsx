e3d29a2b76b3531d572f04491e3d44e088a7d219
ferdous-al-quraeshi <ecopavel@gmail.com>
2018-08-10 11:14:24 +0600

    applying 'ingle-source-of-truth' & removing local states

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,7 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // states of this component
-  state = {
-    value: this.props.counter.value,
-    tags: [
-      {
-        id: 1,
-        el: "tag1"
-      },
-      {
-        id: 2,
-        el: "tag1"
-      },
-      {
-        id: 3,
-        el: "tag1"
-      },
-      {
-        id: 4,
-        el: "tag1"
-      }
-    ]
-  };
+  // Now it's made a controlled (stateless) component to implement 'Single Source of Truth' pattern
 
   /* a classic approach for 'binding' event handlers */
   // constructor() {
@@ -35,23 +14,17 @@ class Counter extends Component {
       <div className="jumbotron">
         <h4>Counter# {this.props.counter.id}</h4>
         <button
-          onClick={this.handleDecrement}
+          onClick={() => this.props.onDecrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           -
         </button>
         <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           +
-        </button>
-        <button
-          onClick={this.handleReset}
-          className="btn btn-light btn-sm ml-2"
-        >
-          Reset
         </button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
@@ -64,18 +37,6 @@ class Counter extends Component {
   }
 
   /* helper methods */
-  handleIncrement = () => {
-    // the arrow function approach that auto inherited (NOT re-bind) the 'this'
-    this.setState({ value: this.state.value + 1 });
-  };
-  handleDecrement = () => {
-    // the arrow function approach that auto inherited (NOT re-bind) the 'this'
-    this.setState({ value: this.state.value - 1 });
-  };
-  handleReset = () => {
-    // the arrow function approach that auto inherited (NOT re-bind) the 'this'
-    this.setState({ value: 0 });
-  };
   renderTags() {
     if (this.state.tags.length === 0)
       return (
@@ -95,17 +56,18 @@ class Counter extends Component {
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes +=
-      this.state.value === 0
+      this.props.counter.value === 0
         ? "warning"
-        : this.state.value < 0
+        : this.props.counter.value < 0
           ? "danger"
-          : this.state.value > 0
+          : this.props.counter.value > 0
             ? "primary"
             : 0;
     return classes;
   }
   formatCount() {
-    const { value } = this.state;
+    // object destructuring to read the value property of the 'state'
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 
+let style = {
+  resetBtn: {
+    position: "fixed",
+    top: "80px",
+    left: "50%"
+  }
+};
+
 class Counters extends Component {
   state = {
     name: "Counters",
@@ -13,18 +21,46 @@ class Counters extends Component {
   };
 
   /* Children's event handlers */
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value--;
+    this.setState({ counters });
+  };
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counters });
+  };
   handleDelete = counterId => {
     // return all the counter objects except the one with the given id (the del btn of which is clicked)
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters }); // polluting the state of the counters
   };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
 
   render() {
     return (
       <div>
+        <button
+          style={style.resetBtn}
+          onClick={this.handleReset}
+          className="btn btn-secondary ml-3"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
+            onDecrement={this.handleDecrement}
+            onIncrement={this.handleIncrement}
             onDelete={this.handleDelete}
             counter={counter} // this 'counter' object encapsulates all the props / data within it and save us from redundant explicit prop declaration
           />
